8d5a70eb0b20fa8607f81d0ba17fb9eda9f49f96
Mario Inga <mario21ic@gmail.com>
2020-03-22 23:24:24 -0500

    feat(global) adding some java examples

--- java/2_replace-temp-query/src/main/java/com/example/App.java
+++ java/2_replace-temp-query/src/main/java/com/example/App.java
@@ -4,8 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        // MyClass myclass = new MyClass(11, 32.11);
-        MyClassSolved myclass = new MyClassSolved(11, 32.11);
+        MyClass myclass = new MyClass(11, 32.11);
+        // MyClassSolved myclass = new MyClassSolved(11, 32.11);
         System.out.println(myclass.calcPrice());
     }
 }
